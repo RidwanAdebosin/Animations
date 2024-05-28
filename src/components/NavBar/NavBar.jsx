dd3ec5a6ad7e0bf4fcd9835b2e9531b5881362af
RidwanAdebosin <adebosinadewale@gmail.com>
2024-05-28 22:23:11 +0100

    state added to control the hambugger menu

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,15 +1,19 @@
 import './NavBar.css';
 import Hamburger from 'hamburger-react';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 const NavBar = () => {
+    const [hambuggerIsOpen, setHambuggerIsOpen] = useState(false);
+
+
     return (
         <header className="header-bar">
             <div className="top-bar">
             <h1>APRIL <span>GROUP</span></h1>
-            <Hamburger />
+            <Hamburger toggled={hambuggerIsOpen} toggle={setHambuggerIsOpen} aria-label="Toggle Menu"/>
             </div>
-            <nav className="nav-bar">
+            <nav className={`nav-bar ${hambuggerIsOpen ? 'open': ""}`}>
             <Link to="/properties"className='link'>PROPERTIES</Link>
             <Link to="/investors" className='link'>INVESTORS</Link>
             <Link to="/about"className='link'>ABOUT</Link>
